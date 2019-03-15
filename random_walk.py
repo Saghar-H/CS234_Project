d945aeb6999b8fa1cf06086df37270858fa3a084
siamak <siamaksha1@gmail.com>
2019-03-14 22:48:05 -0700

    P and D

--- random_walk.py
+++ random_walk.py
@@ -19,16 +19,52 @@ def init_env(env_name, seed):
     env.seed(seed)
     return env
 
+def compute_P(transition_probs, num_actions, num_states):
+    ret = np.zeros((num_states, num_states))
+    for s in transition_probs.keys():
+        for a in transition_probs[s]:
+            for tup in transition_probs[s][a]:
+                sp = tup[1]
+                p_sasp = tup[0]
+                ret[s,sp] += 1.0/num_actions * p_sasp
+
+    return ret
+
+
 
 done = False
 seed = 1358
 env_name = 'WalkFiveStates-v0'
 env = init_env(env_name, seed)
 
-num_episodes = 10
+num_episodes = 1
 G = []
 transition_probs = env.env.P
+P = compute_P(transition_probs, env.action_space.n, env.observation_space.n)
 gamma = .5
+
+
+# def run_env_episodes(num_episodes):
+#     D = np.zeros(env.observation_space.n)
+#     total_steps = 0
+#     for ep in range(num_episodes):
+#         env.reset()
+#         done = False
+#         while not done:
+#             #env.render()
+#             state, reward, done, info = env.step(random.randint(0, env.action_space.n - 1))
+#             D[state] +=1
+#             total_steps +=1
+#     #print('discounted rewards:{0}'.format(G))
+#     #print ("average score: ", sum(collected_rewards) / num_episodes)
+#     print(D / total_steps, total_steps)
+#     return np.diag(D / total_steps)
+
+# print('Generate D matrix...')
+# D = run_env_episodes(100000)
+# print('Done finding D!')
+
+D = np.diag([0.12443139 ,0.24981192 ,0.25088312, 0.25018808 ,0.12468549])
 
 collected_rewards = []
 for ep in range(num_episodes):
@@ -37,7 +73,7 @@ for ep in range(num_episodes):
     done = False
     episode_reward = 0
     while not done:
-        env.render()
+        #env.render()
         state, reward, done, info = env.step(random.randint(0, env.action_space.n - 1))
         ep_rewards.append(reward)
         episode_reward += reward
