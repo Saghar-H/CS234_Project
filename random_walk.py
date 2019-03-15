70f81f7e420ab599aae05823674b8b3f1b34a13f
siamak <siamaksha1@gmail.com>
2019-03-14 21:43:47 -0700

    merge random walk and frozen lake

--- random_walk.py
+++ random_walk.py
@@ -1,30 +1,53 @@
 import gym
 import gym_walk
+import pdb
 import numpy as np
+import random
+from pprint import pprint
 
 
-env = gym.make('WalkSevenStates-v0')
+def get_discounted_rewards(episode_rewards, gamma):
+    discounted_rewards = [0] * (len(episode_rewards) + 1)
+    for i in range(len(episode_rewards)-1,-1,-1):
+        discounted_rewards[i] = discounted_rewards[i+1] * gamma + episode_rewards[i]
+    return discounted_rewards[:-1]
+    
+def init_env(env_name, seed):
+    env = gym.make(env_name)
+    env.reset()
+    random.seed(seed)
+    env.seed(seed)
+    return env
 
-num_episodes = 20
-num_steps_per_episode = 200
+
+done = False
+seed = 1358
+env_name = 'WalkFiveStates-v0'
+env = init_env(env_name, seed)
+
+num_episodes = 10
+G = []
+transition_probs = env.env.P
+gamma = .5
 
 collected_rewards = []
-for i in range(num_episodes):
-    s = env.reset()
-    print ("starting new episode")
-    env.render()
-    print ("started")
-    total_reward = 0
+for ep in range(num_episodes):
+    ep_rewards = []
+    env.reset()
     done = False
-    for j in range(num_steps_per_episode):
-        a = np.random.randint(env.action_space.n)
-        s1, reward, done, _ = env.step(a)
+    episode_reward = 0
+    while not done:
         env.render()
-        total_reward += reward
-        s = s1
-        if done:
-            break
-    collected_rewards.append(total_reward)
-    print ("total reward ", total_reward, " after episode: ", j)
+        state, reward, done, info = env.step(random.randint(0, env.action_space.n - 1))
+        ep_rewards.append(reward)
+        episode_reward += reward
+    ep_discountedrewards = get_discounted_rewards(ep_rewards, gamma)
+    G.append(ep_discountedrewards)
+    collected_rewards.append(episode_reward)
+    print ("episode total reward ", episode_reward, " after episode: ", ep)
+
+print('discounted rewards:{0}'.format(G))
 print ("average score: ", sum(collected_rewards) / num_episodes)
 print("#########")
+
+
