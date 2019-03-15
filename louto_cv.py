618808df48c78bb01468814121f03c27aafbcd0c
Shima Salimi Tari <shsalimi@microsoft.com>
2019-03-14 22:59:20 -0700

    cv gradient

--- louto_cv.py
+++ louto_cv.py
@@ -2,16 +2,18 @@ import numpy as np
 
 def compute_cv_gradient(phi, theta, gamma, lstd_lambda, P, V, D):
     I = np.eye(len(P), len(P[0]))
+    phi_t = phi.transpose()
     print(I)
     print(P)
     print(gamma*lstd_lambda*P)
     print(I - gamma*lstd_lambda*P)
     inv1 = np.linalg.inv(I - gamma*lstd_lambda*P)
-    psi = np.dot(np.dot(np.dot(phi.transpose(), D), inv1), I - gamma*P) 
+    psi = np.dot(np.dot(np.dot(phi_t, D), inv1), I - gamma*P) 
     print(psi)
     inv2 = np.linalg.inv(np.dot(psi, phi))
     H = np.dot(np.dot(phi, inv2), psi)
     print(H)
+	gradient_psi = gamma * phi_t @ D
                
 if __name__ == '__main__':
     nS = 5
