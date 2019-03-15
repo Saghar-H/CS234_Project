55c92025dc81973b2fa979c1d0ce04f54b0766a9
Shima Salimi Tari <shsalimi@microsoft.com>
2019-03-15 01:21:17 -0700

    fix inverses

--- random_walk.py
+++ random_walk.py
@@ -35,7 +35,6 @@ def compute_cv_gradient(phi, theta, gamma, lstd_lambda, P, V, D):
     I = np.eye(len(P), len(P[0]))
     phi_t = phi.transpose()
     V_t = V.transpose()
-    inv_P = np.linalg.inv(P)
     I_gamma_P = I - gamma*P
     print(I)
     print(P)
@@ -43,23 +42,24 @@ def compute_cv_gradient(phi, theta, gamma, lstd_lambda, P, V, D):
     print(I - gamma*lstd_lambda*P)
     inv1 = np.linalg.inv(I - gamma*lstd_lambda*P)
     psi = phi_t @ D @ inv1 @ I_gamma_P
+    print('*****---- psi ----*****')
     print(psi)
     inv2 = np.linalg.inv(psi @ phi)
     H = phi @ inv2 @ psi
     d = np.diag(np.diag(I - H))
-    d_inv2 = np.linalg.inv(np.linalg.inv(d))
-    d_inv3 = np.linalg.inv(d_inv2)
     print('*****----H----*****')
     print(H)
     print('*****----diag(I-H)----*****')
     print(d)
+    d_inv2 = np.linalg.inv(np.linalg.inv(d))
+    d_inv3 = np.linalg.inv(d_inv2)
     print('*****----(diag(I-H))^(-2)----*****')
     print(d_inv2)
     print('*****----(diag(I-H))^(-3)----*****')
     print(d_inv3)
-    psi_gradient = gamma * phi_t @ D @ inv1 @ inv_P @ inv1 @ I_gamma_P
+    psi_gradient = gamma * phi_t @ D @ inv1 @ P @ inv1 @ I_gamma_P
     inv3 = np.linalg.inv(psi_gradient @ phi)
-    H_gradient = phi @ inv2 @ (psi_gradient - inv3 @ inv2 @ psi)
+    H_gradient = phi @ inv2 @ (psi_gradient - psi_gradient @ phi @ inv2 @ psi)
     diag_H_gradient = np.diag(np.diag(H_gradient))
     print(H_gradient)
     H_V = phi @ theta
@@ -179,14 +179,16 @@ print('episode loss:{0}'.format(loss))
 print ("average loss: ", sum(loss) / num_episodes)
 print("#########")
 print("#### Compute CV Gradient #####")
-nS = 5
-nF = 3
-phi = np.random.rand(nS, nF)
-theta = np.random.rand(nF, 1)
-lstd_lambda = 0.5
-gamma = 1
-P = np.random.rand(nS, nS)
-V = np.random.rand(nS, 1)
-D = np.random.rand(nS, nS)
-compute_cv_gradient(phi, theta, gamma, lstd_lambda, P, V, D)
+#nS = 5
+#nF = 3
+#phi = np.random.rand(nS, nF)
+#theta = np.random.rand(nF, 1)
+#P = np.random.rand(nS, nS)
+#V = np.random.rand(nS, 1)
+#D = np.random.rand(nS, nS)
+print(P)
+#print(np.linalg.inv(P + np.ones(len(P), len(P)) * 1e-15))
+print('---------theta------------')
+print(theta)
+compute_cv_gradient(Phi, theta, gamma, lambda_, P, V, D)
 print("#########")
