b9b237d226affc03e46fe0eaac47082f6dda1439
Saghar Hosseini <saghar.hossaini@gmail.com>
2019-03-16 16:42:44 -0700

    add gym random walk env to repo

---
+++ gym-walk/__init__.py
@@ -0,0 +1,26 @@
+from gym.envs.registration import register
+
+register(
+    id='WalkFiveStates-v0',
+    entry_point='gym_walk.envs:WalkEnv',
+    kwargs={'n_states': 5},
+    timestep_limit=100,
+    reward_threshold=1.0,
+    nondeterministic=True,
+)
+register(
+    id='WalkSevenStates-v0',
+    entry_point='gym_walk.envs:WalkEnv',
+    kwargs={'n_states': 7},
+    timestep_limit=100,
+    reward_threshold=1.0,
+    nondeterministic=True,
+)
+register(
+    id='WalkTwentyOneStates-v0',
+    entry_point='gym_walk.envs:WalkEnv',
+    kwargs={'n_states': 21},
+    timestep_limit=1000,
+    reward_threshold=1.0,
+    nondeterministic=True,
+)
---
+++ gym-walk/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gym_walk',
+    version='0.0.1',
+    description='Gym walk environment - useful to replicate Random Walk experiments',
+    url='https://github.com/rldm/gym-walk',
+    author='Miguel Morales',
+    author_email='mimoralea@gmail.com',
+    packages=['gym_walk'],
+    license='MIT License',
+    install_requires=['gym'],
+)
