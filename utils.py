d387b861715f971ea0e6723669a2b1d8518d18c1
Saghar Hosseini <saghar.hossaini@gmail.com>
2019-09-03 23:35:18 -0700

    Added H_jj gradient

--- utils.py
+++ utils.py
@@ -4,6 +4,15 @@ from pprint import pprint
 def compute_A_inv_gradient(A, b, z, Phi)
 def compute_b_grandient(b)
 def compute_z_gradient()
-def compute_hjj_gradient()
+def compute_hjj_gradient(Phi, gamma, A, b,  A_inv, z, cur_state, next_state):
+    z_grad = compute_z_gradient(gamma, lambda_, Phi)
+    A_inv_grad = compute_A_inv_gradient(A, b, z, Phi)
+    term1 = Phi[cur_state, :]-gamma* Phi[next_state, :]
+    term2 = term1 @ A_inv
+    term3 = term2 @ z_grad
+    term4 = term1 @ A_inv_grad
+    term5 = term4 @ z
+    return term3 + term5
+
 def compute_epsilon_lambda_gradient()
 def compute_lcv_h_gradient()
