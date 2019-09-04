31ec4ac63b04804454bfcde2eb5daac516746a5d
siamak <siamaksha1@gmail.com>
2019-09-04 00:10:22 -0700

    add annotation to the functions

--- utils.py
+++ utils.py
@@ -3,17 +3,26 @@ from pprint import pprint
 
 def compute_A_inv_gradient(
 						   A:np.ndarray, 
-						   b:np.ndarray, 
 						   z:np.ndarray, 
 						   z_grad:np.ndarray, 
 						   Phi:np.ndarray
 						   ) -> np.ndarray:
+	'''
+	inputs: 
+	A: dxd
+	z: dxT
+	z_grad: dx1
+	Phi: Txd
+
+	return: 
+	gradient of A inverse: dxd
+	'''
 
 	A_inv = np.linalg.pinv(A)
 	##Inner sum:
 	sum_inner = 0
 	for i in range(Phi.shape[0]-1):
-		sum_inner += z[i] @ (Phi[i]-Phi[i+1])
+		sum_inner += z[:,i] @ (Phi[i,:]-Phi[i+1,:])
 
 	ret = -1 * A_inv @ (1.0 /(Phi.shape[0]-1) * sum_inner) @ A_inv
 
@@ -22,12 +31,21 @@ def compute_A_inv_gradient(
 def compute_b_grandient(z_grad:np.ndarray, 
 						rewards:np.array
 						) -> np.array:
+
+	'''
+	inputs:
+	z_grad: dxT
+	rewards:Tx1
+
+	return:
+	gradients of b: dx1
+	'''
 	ret = 0
 	sum_inner = 0
-	for i in range(z_grad.shape[0]):
-		sum_inner += z[i] * rewards[i]
+	for i in range(z_grad.shape[1]):
+		sum_inner += z[:,i] * rewards[i]
 
-	return sum_inner * 1.0 / (z_grad.shape[0])
+	return sum_inner * 1.0 / (z_grad.shape[1])
 
 def compute_z_gradient(_lambda, gamma, Phi, ep_states, j):
     result = 0
