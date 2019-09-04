93adeb9a3861dd33daa1519de283b7a2599b90a9
Shima Salimi Tari <shsalimi@microsoft.com>
2019-09-03 22:41:34 -0700

    utils for gradient

---
+++ utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+from pprint import pprint
+
+def compute_A_inv_gradient(A, b, z, Phi)
+def compute_b_grandient(b)
+def compute_z_gradient()
+def compute_hjj_gradient()
+def compute_epsilon_lambda_gradient()
+def compute_lcv_h_gradient()
