ca45a03a6ef0a40bb64db0445df7d581a4f5ac99
Shima Salimi Tari <shsalimi@microsoft.com>
2019-09-04 18:02:22 -0700

    fix indent errors

--- utils.py
+++ utils.py
@@ -48,27 +48,27 @@ def compute_A_inv_gradient(
 						   Phi:np.ndarray,
 						   ep_states: np.ndarray,
 						   ) -> np.ndarray:
-	'''
-	inputs: 
-	A: dxd
-	Z_grad: dxT
-	Phi: sxd
-	ep_states: Tx1
+    '''
+    inputs: 
+    A: dxd
+    Z_grad: dxT
+    Phi: sxd
+    ep_states: Tx1
 
-	return: 
-	gradient of A inverse: dxd
-	'''
+    return: 
+    gradient of A inverse: dxd
+    '''
 
-	A_inv = np.linalg.pinv(A)
-	##Inner sum:
-	sum_inner = 0
-	for i in range(len(ep_states)-1):
-        z_grad =compute_z_gradient(_lambda, gamma, Phi, ep_states, i)
+    A_inv = np.linalg.pinv(A)
+    ##Inner sum:
+    sum_inner = 0
+    for i in range(len(ep_states)-1):
+        z_grad = compute_z_gradient(_lambda, gamma, Phi, ep_states, i)
         sum_inner += z_grad @ (Phi[ep_states[i],:]-Phi[ep_states[i+1],:])
 
-	ret = -1 * A_inv @ (1.0 /(Phi.shape[0]-1) * sum_inner) @ A_inv
+    ret = -1 * A_inv @ (1.0 /(Phi.shape[0]-1) * sum_inner) @ A_inv
 
-	return ret
+    return ret
 
 def compute_b_gradient( 
                         _lambda, 
@@ -78,21 +78,21 @@ def compute_b_gradient(
 						rewards:np.array
 						) -> np.array:
 
-	'''
-	inputs:
-	z_grad: dxT
-	rewards:Tx1
+    '''
+    inputs:
+    z_grad: dxT
+    rewards:Tx1
 
-	return:
-	gradients of b: dx1
-	'''
-	ret = 0
-	sum_inner = 0
-	for i in range(len(ep_states)):
+    return:
+    gradients of b: dx1
+    '''
+    ret = 0
+    sum_inner = 0
+    for i in range(len(ep_states)):
         z_grad = compute_z_gradient(_lambda, gamma, Phi, ep_states, i)
-		sum_inner += z_grad * rewards[i]
+        sum_inner += z_grad * rewards[i]
 
-	return sum_inner * 1.0 / (Phi.shape[0])
+    return sum_inner * 1.0 / (Phi.shape[0])
 
 def compute_eps_t(Phi, theta, gamma, reward, ep_states, j):
     cur_state, next_state = ep_states[j], ep_states[j+1]
